9e6edaceb67a6fb2c2d8b3a5dd2b233ec6cf6141
youngwill500 <blackmanblacktie@gmail.com>
2020-02-07 22:43:57 -0500

    Added a rando under LoginPage

--- src/test/java/com/hrms/pages/LoginPage.java
+++ src/test/java/com/hrms/pages/LoginPage.java
@@ -10,5 +10,6 @@ public class LoginPage extends BaseClass{
 	public WebElement username=driver.findElement(By.id("txtUsername"));
 	public WebElement password=driver.findElement(By.id("txtPassword"));
 	public WebElement loginBtn=driver.findElement(By.id("btnLogin"));
+	public WebElement ToginBtn=driver.findElement(By.id("btnLogin"));
 	
 }
